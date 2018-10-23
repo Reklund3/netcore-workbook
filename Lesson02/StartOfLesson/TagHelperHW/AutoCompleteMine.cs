86d0f88e12d25286a9bcb10cf24d3bb1cb8bc8eb
Robert Eklund <robert.eklund3@gmail.com>
2018-10-23 16:58:53 -0500

    Push Lesson 2 Homework up

---
+++ Lesson02/StartOfLesson/TagHelperHW/AutoCompleteMine.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+
+namespace TagHelperHW
+{
+    [HtmlTargetElement("input")]
+    public class AutoCompleteMine : TagHelper
+    {
+        [HtmlAttributeName("my-auto-complete")]
+        public string AutoComp { get; set; }
+        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (AutoComp == "disabled" || AutoComp == "off" || AutoComp == "false")
+            {
+                output.Attributes.SetAttribute("autocomplete", "off");
+            }
+            else if (AutoComp == "enabled" || AutoComp == "on" || AutoComp == "true")
+            {
+                output.Attributes.SetAttribute("autocomplete", "on");
+            }
+        }
+        
+    }
+}
